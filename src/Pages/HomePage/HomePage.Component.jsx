6c73c811a43aedcfba0c0ca82ad4f0bcfa6ca637
Mbarak Mbigo <mbarak.mbigo@andela.com>
2018-01-18 20:09:07 +0300

    chore(setup): setup application structure
    - setup webpack
    - setup styles
    - setup server

---
+++ src/Pages/HomePage/HomePage.Component.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// styling
+import './HomePage.css';
+
+class HomePage extends Component {
+  render () {
+    return (
+      <div className="home-container">
+        <h1>Home Page BucketList</h1>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Components
+import HomePage from './Pages/HomePage/HomePage.Component.jsx';
+
+ReactDOM.render(
+  <HomePage />,
+  document.getElementById('root')
+);
